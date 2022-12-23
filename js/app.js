f1cce548237eb841874c61e46cbd00d8a630be9f
zhanyar004 <119080464+zhanyar004@users.noreply.github.com>
2022-12-23 17:38:06 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,75 @@
+// Dom7
+// var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+	theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+	id: 'gq.sizu.app',
+	root: '#app',
+	theme: theme,
+	data: function () {
+		return {
+			user: {
+				firstName: 'John',
+				lastName: 'Doe',
+			},
+		};
+	},
+	methods: {
+		helloWorld: function () {
+			app.dialog.alert('Hello World!');
+		},
+	},
+	routes: routes,
+	popup: {
+		closeOnEscape: true,
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	},
+	vi: {
+		placementId: 'pltd4o7ibb9rc653x14',
+	},
+	dialog: {
+		// set default title for all dialog shortcuts
+		title: 'My App',
+		// change default "OK" button text
+		buttonOk: 'Done'
+	},
+});
+
+window.onload = function () {
+	WebPullToRefresh.init({
+		loadingFunction: exampleLoadingFunction
+	});
+};
+
+// Just an example loading function that returns a
+// promise that WebPullToRefresh can use.
+var exampleLoadingFunction = function () {
+	location.reload();
+};
+
+
+//Follow us on twitter
+app.dialog.confirm("Subscribe to our channel to get notifications in time.","InstaTechHD",function(){
+window.location.href="https://youtube.com/instatechhd";
+});
+
+
+ 
+
---
+++ js/link.js
@@ -0,0 +1,128 @@
+var code, getIt, errorMessage;
+
+getIt = document.getElementById("get");
+errorMessage = document.getElementById("P-For-Errors");
+
+getIt.addEventListener("click", callLinks);
+
+function callLinks (){
+  code = document.getElementById("code").value;
+
+  switch (code){
+    case "302":
+      window.location.href = "https://is.gd/BR5sTo"
+      break;
+
+      
+          case "593":
+      window.location.href = "https://is.gd/BR5sTo";
+      break;
+
+                case "504":
+      window.location.href = "https://is.gd/BR5sTo";
+      break;
+      
+      
+          case "509":
+      window.location.href = "https://is.gd/oT9ubq";
+      break;
+      
+      
+                case "54":
+      window.location.href = "https://is.gd/mSsglQ";
+      break;
+      
+      
+                     case "187":
+      window.location.href = "https://is.gd/LIIaly";
+      break;
+      
+      
+                           case "426":
+      window.location.href = "https://is.gd/Y8TTZD";
+      break;
+      
+                                 case "228":
+      window.location.href = "https://is.gd/GIHEK1";
+      break;
+      
+      
+                                       case "18":
+      window.location.href = "https://is.gd/IVjugr";
+      break;
+      
+      
+                           case "160":
+      window.location.href = "https://is.gd/X1WlNn";
+      break;
+      
+      
+                                 case "431":
+      window.location.href = "https://is.gd/nPczSj";
+      break;
+      
+      
+          case "101":
+      window.location.href = "https://is.gd/sGiEUQ";
+      break;
+      
+      
+              case "319":
+      window.location.href = "https://is.gd/f6rGgf";
+      break;
+      
+      
+                    case "117":
+      window.location.href = "https://is.gd/J0vHRU";
+      break;
+      
+      
+                          case "105":
+      window.location.href = "https://is.gd/J0vHRU";
+      break;
+      
+     
+                                case "793":
+      window.location.href = "https://is.gd/nIEoEz";
+      break;
+      
+      
+                                      case "605":
+      window.location.href = "https://is.gd/dmtIdN";
+      break;
+      
+                                            case "476":
+      window.location.href = "https://is.gd/xS8ZHj";
+      break;
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      case "":
+        errorMessage.innerHTML = "تکایە ژمارەکە بنووسە";
+        errorMessage.style = "text-align:center;font-size:25px;color: semibold;font-family: myFirstFont ;";
+        break;
+  
+      default:
+        errorMessage.innerHTML = "ئەم ژمارەیە تۆمار نەکراوە";
+        errorMessage.style = "text-align:center;font-size:25px;color: semibold;font-family: myFirstFont ;";
+        break;
+  
+    }
+  }
